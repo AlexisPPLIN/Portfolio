0a3a48edc25a410255ed99ae63e0a76df5a852e3
AlexisPPLIN <alexis.poupelin@protonmail.com>
2021-04-20 22:05:10 +0200

    First commit

---
+++ components/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="flex fixed w-full items-center justify-between px-6 h-16 bg-white text-gray-700 border-b border-gray-200 z-10">
+        <button class="p-2 bg-purple-600 hover:bg-purple-700">
+            Hello world
+        </button>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
